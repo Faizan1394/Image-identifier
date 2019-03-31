3809906bcdce6e239ee0a914e642741fc150d53f
Christien-Soosaipillai <christien2011@hotmail.ca>
2019-03-31 18:46:34 -0400

    fixed UI, and login auth

--- Item_Identifier/app/src/main/java/com/example/christien/item_identifier/HomeActivity.java
+++ Item_Identifier/app/src/main/java/com/example/christien/item_identifier/HomeActivity.java
@@ -1,18 +1,22 @@
 package com.example.christien.item_identifier;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Button camButton;
+    private Button logoutButton;
     private ImageView image;
 
     @Override
@@ -22,6 +26,7 @@ public class HomeActivity extends AppCompatActivity {
 
 
         camButton = (Button) findViewById(R.id.cameraButton);
+        logoutButton = (Button) findViewById(R.id.logoutButton);
         image = (ImageView) findViewById(R.id.picture);
 
         camButton.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +34,32 @@ public class HomeActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(intent,0);
+            }
+        });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(HomeActivity.this);
+                alertBuilder.setMessage("Are you sure you want to log out?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(HomeActivity.this, "Logging out", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = alertBuilder.create();
+                //alert.setTitle("");
+                alert.show();
             }
         });
 
