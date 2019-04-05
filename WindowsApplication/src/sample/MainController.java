d4e156ff431b1fc9a87ed40e33d2c37914b9626b
miral-panchal <miral.panchal@uoit.net>
2019-04-04 23:23:33 -0400

    Image Loading

--- WindowsApplication/src/sample/MainController.java
+++ WindowsApplication/src/sample/MainController.java
@@ -1,9 +1,12 @@
 package sample;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +17,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
 
 
 public class MainController implements Initializable {
@@ -29,7 +33,14 @@ public class MainController implements Initializable {
     public void close(ActionEvent e) {
 //       System.exit(0);
 
-        img.setImage(new Image("readImage.png"));
+
+        File file = new File("src\\resources\\readImage.png");
+        try {
+
+            BufferedImage bf = ImageIO.read(file);
+            Image image = SwingFXUtils.toFXImage(bf,null);
+            img.setImage(image);
+        }catch (Exception ex) {ex.printStackTrace();}
     }
 
     public void logout(ActionEvent e) {
